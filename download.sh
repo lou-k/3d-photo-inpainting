192e357e8a5ca6d99eabab8fcd133e9438514e1d
Oscar <tangqh@sea.com>
2023-02-20 17:57:36 +0800

    perf: download models from google drive instead

--- download.sh
+++ download.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-fb_status=$(wget --spider -S https://filebox.ece.vt.edu/ 2>&1 | grep  "HTTP/1.1 200 OK")
 
 mkdir checkpoints
 
-echo "downloading from filebox ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
+echo "downloading models ..."
+!gdown 1oPD-0Ni5iNqf5xFam55z24JmRk4nfvOe # color-model.pth
+!gdown 1oRwIFtwV3VP3cAhQhZAdMrF3XwWuR0_9 # depth-model.pth
+!gdown 1oRevcIYAdlIDdytjjTAXJyHX6YwrDGHB # edge-model.pth
+!gdown 1oUM3KNJ-E6mVsYauNInf45WSuhy1c5Ec # model.pt
 
 mv color-model.pth checkpoints/.
 mv depth-model.pth checkpoints/.
@@ -19,7 +18,7 @@ git clone https://github.com/compphoto/BoostingMonocularDepth.git
 mkdir -p BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 
 echo "downloading mergenet weights ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/latest_net_G.pth
+!gdown 1oPBsmT3jo-eAFJBY4ASf0nn7LaMQlIal # latest-net-G.pth
 mv latest_net_G.pth BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 wget https://github.com/intel-isl/MiDaS/releases/download/v2/model-f46da743.pt
 mv model-f46da743.pt BoostingMonocularDepth/midas/model.pt
